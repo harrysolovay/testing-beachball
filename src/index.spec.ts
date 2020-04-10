0b84e7bf39a099979adb20397878a4605fc328ec
Harry Solovay <harrysolovay@gmail.com>
2020-04-09 19:50:31 -0700

    chore: first commit

---
+++ src/index.spec.ts
@@ -0,0 +1,8 @@
+import {api} from ".";
+
+describe("test block 1", () => {
+  it("test 1", () => {
+    expect.assertions(1);
+    expect(api()).toStrictEqual("Hello World");
+  });
+});
---
+++ src/tests/api.ts
@@ -0,0 +1,8 @@
+import {api} from "..";
+
+describe("test block 2", () => {
+  it("test 1", () => {
+    expect.assertions(1);
+    expect(api()).toStrictEqual("Hello World");
+  });
+});
